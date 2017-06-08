7fc391adfcefb56a7d00db82ff8d55ef824c39ad
Hardip Patel <hardip.patel@anu.edu.au>
2017-06-08 12:01:37 +1000

    Update albacore_poretools.sh

--- albacore_poretools.sh
+++ albacore_poretools.sh
@@ -57,10 +57,12 @@ module unload python3/3.5.2 albacore/1.1.2
 ##remove input directory
 rm -rf $inputdir_local
 
-###fastq files are $outputdir_local/workspace/*.fastq
-###fast5 files are in $outputdir_local/workspace/0
+###fastq files are $outputdir_local/workspace/*.fastq. Normally it should only be 1 fastq file because of very high number for -n parameter above in albacore run
+###fast5 files are in $outputdir_local/workspace/0. Normally it should only be 0 because of very high number in -q parameter above in albacore run
+
 ### you can add extra commands here for additional analysis of fastq or fast5 files
-###if you can you should use $outputdir_local as the output directory for analysis results
+###You should use $outputdir_local as the output directory for analysis/results. This will ensure that results are compressed and transferred back to the network attached storage.
+### There are lots of files generated as a result of analysis and it will exceed you inode quota on NCI so this is the best possible way to get around it.
 
 ##tar results folder
 command=(tar czf $PBS_JOBFS/$dataid.results.tar.gz -C $PBS_JOBFS/ results_$dataid)
@@ -71,4 +73,8 @@ execute_command command[@] $dataid.tar $outputdir/$dataid.tar.done 1
 command=(rsync -a $PBS_JOBFS/$dataid.results.tar.gz $outputdir/)
 execute_command command[@] $dataid.copy2nas $outputdir/$dataid.copy2nas.done 1
 
+###once all results are copied back to the network storage then one can extract FASTQ files only as follows:
+
+###tar xvzf $outputdir/$dataid.results.tar.gz results_$dataid/workspace/*.fastq
+###the above command will create results_$dataid/workspace/*.fastq file in the current working directory
 
