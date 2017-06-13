ad7785b84f389349193659b73fbb7bcb64329304
Hardip Patel <hardip.patel@anu.edu.au>
2017-06-13 20:11:01 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -50,13 +50,30 @@ module load python/2.7.11 python/2.7.11-matplotlib gcc/system mpi4py petsc/3.4.3
 export HDF5_VERSION=1.8.10
 mkdir -p /short/nk44/software/poretools/0.6.0/lib/python2.7/site-packages
 export PYTHONPATH=/short/nk44/software/poretools/0.6.0/lib/python2.7/site-packages
+cd poretools
 python2.7 setup.py install --prefix=/short/nk44/software/poretools/0.6.0
 python2.7 setup.py install --prefix=/short/nk44/software/poretools/0.6.0
 createmodulefile poretools 0.6.0 python/2.7.11
 echo "soft-prereq python/2.7.11-matplotlib" >>/short/nk44/modules/poretools/0.6.0
 echo "prepend-path PYTHONPATH /short/nk44/software/poretools/0.6.0/lib/python2.7/site-packages" >>/short/nk44/modules/poretools/0.6.0
 module unload python/2.7.11 python/2.7.11-matplotlib gcc/system mpi4py petsc/3.4.3 openmpi/1.6.3 hdf5/1.8.10p
+cd ..
 
-
+##install ngmlr
+git clone https://github.com/philres/ngmlr.git
+mkdir -p ngmlr/build
+cd ngmlr/build
+module load  intel-cc/16.0.3.210  intel-fc/16.0.3.210 gcc/4.9.0 zlib/1.2.8 cmake/3.6.2
+cmake -DSTATIC=OFF ..
+make
+cd ../../
+module unload intel-cc/16.0.3.210  intel-fc/16.0.3.210 gcc/4.9.0 zlib/1.2.8 cmake/3.6.2
+mkdir -p /short/nk44/software/ngmlr/0.2.4/bin
+cp bin/ngmlr-0.2.4/ngmlr /short/nk44/software/ngmlr/0.2.4/bin/
+createmodulefile ngmlr 0.2.4 intel-cc/16.0.3.210
+echo "soft-prereq intel-fc/16.0.3.210" >>/short/nk44/modules/ngmlr/0.2.4
+echo "soft-prereq gcc/4.9.0" >>/short/nk44/modules/ngmlr/0.2.4
+echo "soft-prereq zlib/1.2.8" >>/short/nk44/modules/ngmlr/0.2.4
+echo "soft-prereq cmake/3.6.2" >>/short/nk44/modules/ngmlr/0.2.4
 
 
